4a6a312d9f897482ccdccb393c1d0ae3da8ae3a9
김유나 <yunae426@gmail.com>
2022-07-19 15:22:37 +0900

    220719

---
+++ 2005_파스칼삼각형.py
@@ -0,0 +1,21 @@
+T = int(input())
+
+for t in range(T):
+    triangle = []
+    rows = int(input())
+    
+    print(f'#{t+1}')
+    # 입력된 행의 개수 만큼 이중 리스트
+    for row in range(rows): 
+        triangle.append([])
+        for column in range(row+1):
+            # 열 번호가 맨 앞이거나 맨 뒤일 때 1을 원소로 추가
+            if( column == 0 or column == row):
+                triangle[row].append(1)
+                print(triangle[row][column],end =" ")
+            # 그 외의 경우에는 [row-1][column-1],[row-1][column]의값을 더해 준 값 추가
+            else:
+                triangle[row].append(triangle[row-1][column-1]+triangle[row-1][column])
+                print(triangle[row][column],end =" ")
+        print()         
+               
