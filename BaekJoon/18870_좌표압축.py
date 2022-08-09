da1a828a8490fb916cb8eac90a991109adc8902a
김유나 <yunae426@gmail.com>
2022-08-09 17:52:50 +0900

    algorithm

--- BaekJoon/18870_좌표압축.py
+++ BaekJoon/18870_좌표압축.py
@@ -1,14 +1,14 @@
-N = int(input())
-
-arr = list(map(int, input().split()))
-cnt_arr = []
-
-for i in range(len(arr)):
-
-
-
-for n in arr:
-    print(arr_sorted.index(n), end= " ")
+# N = int(input())
+#
+# arr = list(map(int, input().split()))
+# cnt_arr = []
+#
+# for i in range(len(arr)):
+#
+#
+#
+# for n in arr:
+#     print(arr_sorted.index(n), end= " ")
 
 
 
