db61a0dee0d7d53d1082c95fe5488e652cd949cd
yuna kim <yunae426@gmail.com>
2022-09-22 00:04:09 +0900

    algorithm

--- SWEA/1244_최대상금.py
+++ SWEA/1244_최대상금.py
@@ -2,31 +2,43 @@ import sys
 sys.stdin = open('1244_input.txt', 'r')
 
 # 순열을 생성하는 함수
-def change(s):
+def change(n, r, cnt, C):
     global maxV
 
-    if s == K:
-        # print(nums)
+    if cnt == C:
         res = int(''.join(nums))
         if res > maxV:
             maxV = res
             return
+
+    elif n == r:
+        # 반복횟수가 길이보다 더 클 경우에는 횟수만큼 도달하지 못하므로 더 돌려줘야 함
+        if C > len(nums):
+            change(0, r, cnt, C-len(nums))
+            return
+        return
+
     else:
-        for j in range(s, len(nums)):
-            nums[s], nums[j] = nums[j], nums[s]
-            change(s+1)
-            nums[j], nums[s] = nums[s], nums[j]
+        for j in range(n, len(nums)):
+            nums[n], nums[j] = nums[j], nums[n]
+            # 자기자신을 바꾸는 경우에는 카운트를 해주지 말자,,
+            if n == j:
+                change(n + 1, r, cnt, C)
+            else:
+                change(n + 1, r, cnt + 1, C)
+            nums[j], nums[n] = nums[n], nums[j]
 
 
 T = int(input())
 
 for test_case in range(1, T+1):
-    nums, K = input().split()
+    nums, C = input().split()
     nums = list(nums)
-    K = int(K)
+    C = int(C)
     maxV = 0
 
-    change(0)
+
+    change(0, len(nums), 0, C)
     print(f'#{test_case} {maxV}')
 
 
--- SWEA/5188_최소합.py
+++ SWEA/5188_최소합.py
@@ -1,8 +1,35 @@
+import sys
+sys.stdin = open('5188_input.txt', 'r')
+
+def solve():
+    i, j = 0, 0
+    sumV = arr[0][0]
+
+    ni, nj = 0, 0
+    while (ni, nj) != (N-1, N-1):
+        ri, rj = ni, nj+1
+        ui, uj = ni+1, nj
+        if 0 <= ri < N and 0 <= rj < N:
+            ni, nj = ri, rj
+        if 0 <= ui < N and 0 <= uj < N:
+            if (ni, nj) == (ri, rj):
+                if arr[ri][rj] > arr[ui][uj]:
+                    ni, nj = ui, uj
+            else:
+                ni, nj = ui, uj
+
+            print(arr[ni][nj], end= ' ')
+            sumV += arr[ni][nj]
+
+    return sumV
+
+
 T = int(input())
 
 for test_case in range(1, T+1):
     N = int(input())
 
     arr = [list(map(int, input().split())) for _ in range(N)]
+    # print(arr)
 
-    d = [(0, 1), (1, 0)]
+    print(solve())
