6dfa8d9d3d16d6827bbac9961d55ebffead90811
yuna kim <yunae426@gmail.com>
2022-09-27 00:07:33 +0900

    algorithm

--- SWEA/5207_이진탐색.py
+++ SWEA/5207_이진탐색.py
@@ -4,20 +4,26 @@ sys.stdin = open('5207_input.txt', 'r')
 T = int(input())
 
 def binarySearch(A, low, high, key):
-    global r, l, check
-
     if low > high:
         return
 
     mid = (low + high) // 2
 
     if A[mid] == key:
-        return 1
+        if low == 0 and high == N-1:
+            return 1
+
+        if visited == [1, 1]:
+            return 1
+
+        return 0
 
     if A[mid] > key:
+        visited[0] = 1
         binarySearch(A, low, mid-1, key)
 
     else:
+        visited[1] = 1
         binarySearch(A, mid+1, high, key)
 
 
@@ -31,7 +37,9 @@ for test_case in range(1,T+1):
 
     cnt = 0
     for b in B:
-        print(binarySearch(A, 0, N-1, b))
+        visited = [0] * 2
+        if binarySearch(A, 0, N-1, b):
+            cnt += 1
 
 
 
