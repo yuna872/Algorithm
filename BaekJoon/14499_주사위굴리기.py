189a321b7061dda4007a28b8d26de4ba89b751f8
yuna872 <yunae426@gmail.com>
2022-10-21 17:42:51 +0900

    algorithm

--- BaekJoon/14499_주사위굴리기.py
+++ BaekJoon/14499_주사위굴리기.py
@@ -2,18 +2,19 @@ def rollDice(m):
     a, b, c, d, e, f = dice
     # 동
     if m == 1:
-        return
+        return [e, b, c, a, f, d]
     if m == 2 :
         return [d, b, c, f, a, e]
+    if m == 3:
+        return [b, f, a, d, e, c]
+    if m == 4:
+        return [c, a, f, d, e, b]
 
-    # 서
-
-    # 남
-    # 북
-
-
-
-
+def solve(x, y):
+    for m in move:
+        dice = rollDice(m)
+        print(dice[0])
+        if dice
     return
 
 N, M, x, y, K = map(int, input().split())
