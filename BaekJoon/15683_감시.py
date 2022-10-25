85b73b125be1ff0f618958177b790a661bf34640
yuna872 <yunae426@gmail.com>
2022-10-25 17:18:25 +0900

    algorithm

--- BaekJoon/15683_감시.py
+++ BaekJoon/15683_감시.py
@@ -1,1 +1,84 @@
+from copy import deepcopy
+from collections import deque
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+# 동, 남, 북, 서
+D = [(), (0, 1), (1, 0), (-1, 0), (0, -1)]
+# 카메라 타입에 따라서 각각 회전할 수 있는 방향 계산해 놓기
+mode = [
+    [],
+    [[1], [2], [3], [4]],
+    [(1, 4), (2, 3)],
+    [(1, 3), (1, 2), (2, 4), (3, 4)],
+    [(1, 2, 3), (1, 2, 4), (2, 3, 4), (1, 3, 4)],
+    [(1, 2, 3, 4)]
+]
+# 감시하는 함수
+def look(arr, k):
+    global minV
+
+    # 모든 카메라에 대한 경우의수를 확인해 보았다면
+    if k == len(cctvs):
+        cnt = 0
+        # 사각지대 영역 개수 세기
+        for i in range(N):
+            for j in range(M):
+                if arr[i][j] == 0:
+                    cnt += 1
+        # 최소값 갱신
+        if cnt < minV:
+            minV = cnt
+        return
+
+    # k번째 카메라 감시하기
+    curi, curj = cctvs[k]
+    # 카메라의 타입 가져오기
+    type = arr[curi][curj]
+
+
+    for direct in mode[type]:
+        # 감시한 흔적은 tmp_arr에 저장하기 (깊은복사)
+        tmp_arr = deepcopy(arr)
+        # 카메라 방향 조정하고 탐색 시작
+        for d in direct:
+            dq = deque()
+            dq.append((curi, curj))
+
+            while dq:
+                i, j = dq.popleft()
+
+                ni, nj = i + D[d][0], j + D[d][1]
+                # 벽에 부딪히지 않으면서
+                if 0 <= ni < N and 0 <= nj < M:
+                    # 감시할 수 있으면 # 로 변경해주기
+                    if tmp_arr[ni][nj] == 0:
+                        tmp_arr[ni][nj] = '#'
+                        dq.append((ni, nj))
+                    # 벽을 만나면 멈추기
+                    elif tmp_arr[ni][nj] == 6:
+                        break
+                    # 다른 카메라가 있을 경우에는 통과
+                    else:
+                        dq.append((ni, nj))
+                # 범위 안에 들어와 있지 않은 경우에도 멈추기
+                else:
+                    break
+        # 다음 카메라로 감시하러 가기
+        look(tmp_arr, k+1)
+
+
+# cctv가 설치된 곳 저장해놓기
+cctvs = []
+for i in range(N):
+    for j in range(M):
+        if 0 < arr[i][j] < 6:
+            cctvs.append((i, j))
+
+minV = 1e9
+# 감시 시작
+look(arr, 0)
+print(minV)
+
+
 
--- BaekJoon/16236_아기상어.py
+++ BaekJoon/16236_아기상어.py
@@ -1,54 +1,80 @@
-import sys
-sys.stdin = open('16236_input.txt', 'r')
+from collections import deque
+
+# 모든 원소에 대해서 최단 거리 구하기
+def bfs(curi, curj):
+    dq = deque()
+    visited = [[-1]*N for _ in range(N)]
+
+    dq.append((curi, curj))
+    visited[curi][curj] = 0
+
+    while dq:
+        i, j = dq.popleft()
+
+        for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < N and 0 <= nj < N:
+                # 아기상어보다 큰 물고기가 있는 영역은 지나갈 수 없어..
+                if water[ni][nj] <= size and visited[ni][nj] == -1:
+                    # 최단 거리를 구하기 위함 => 현재 원소의 visited = 이전 원소의 visited + 1
+                    visited[ni][nj] = visited[i][j] + 1
+                    dq.append((ni, nj))
+
+    # 최단 경로의 정보가 담긴 배열 리턴하기
+    return visited
+
+def meal(visited):
+    global size
+
+    minD = 1e9
+    x, y = -1, -1
+    for i in range(N):
+        for j in range(N):
+            # 아기상어가 먹을 수 있는 크기인지 확인
+            if 0 < water[i][j] < size and visited[i][j] != -1:
+                # 현재 저장된 최단 거리보다 짧은 거리라면
+                if visited[i][j] < minD:
+                    minD = visited[i][j]
+                    x, y = i, j
+
+    return x, y, minD
 
 N = int(input())
+water = [list(map(int, input().split())) for _ in range(N)]
 
-def bfs(i, j):
-    # 아기상어 사이즈
-    size = 2
-    second = 0
-    cnt = 2
-
-    Q = []
-
-    Q.append((i, j))
-
-    while Q:
-        curR, curC = Q.pop(0)
-
-        for dR, dC in d:
-            newR, newC = curR+dR, curC+dC
-
-            if 0 <= newR < N and 0 <= newC < N and sea[newR][newC] <= size and visited[newR][newC] == 0:
-                Q.append((newR, newC))
-                # 크기가 작은 물고기를 만났을때 먹자
-                if sea[newR][newC] < size:
-                    visited[newR][newC] = 1
-                    if sea[newR][newC] == 0:
-                        # 성장하기까지 남은 물고기
-                        cnt -= 1
-                        # 물고기는 먹으면 사라져
-                        sea[newR][newC] = 0
-                        if cnt == 0:
-                            size += 1
-                            cnt = size
-                # 이동시간 +1
-                second += 1
-    if size == 2:
-        return 0
-    else:
-        return second
-
-sea = [list(map(int, input().split())) for _ in range(N)]
-
-# 상하좌우
-d = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-visited = [[0]*N for _ in range(N)]
-
-# 아기상어 시작위치
+# 아기 상어가 출발하는 지점 찾기
 for i in range(N):
     for j in range(N):
-        if sea[i][j] == 9:
-            sea[i][j] = 0
-            print(bfs(i, j))
-            break
+        if water[i][j] == 9:
+            curi, curj = i, j
+            water[curi][curj] = 0
+
+# 상어의 크기
+size = 2
+# 먹은 물고기의 개수
+cnt = 0
+# 시간
+time = 0
+while True:
+    # 현재 위치를 기준으로 모든 원소까지의 최단거리 구하기
+    visited = bfs(curi, curj)
+    # 물고기 먹기
+    curi, curj, minD = meal(visited)
+
+    # 탐색했을 때 최단 거리가 갱신된 적이 없다면 먹을 물고기가 없음
+    if minD == 1e9:
+        break
+    else:
+        # 먹은 물고기 처리해주기
+        water[curi][curj] = 0
+        # 물고기 수 카운트
+        cnt += 1
+        # 이동거리 = 몇초
+        time += minD
+
+        # 크기 만큼의 물고기를 먹었다면 아기 상어의 크기 +1
+        if cnt >= size:
+            cnt = 0
+            size += 1
+
+print(time)
