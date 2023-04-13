a626b97c0d0c46078e97197226d47c6eaa03ff03
yuna <yunae426@gmail.com>
2023-04-13 14:47:08 +0900

    programmers JS

--- Programmers/Lv2/무인도여행.js
+++ Programmers/Lv2/무인도여행.js
@@ -2,37 +2,53 @@ function solution(maps) {
   var answer = [];
   const row = maps.length;
   const col = maps[0].length;
+  let visited = Array(row)
+    .fill()
+    .map(() => Array(col).fill(false));
 
-  console.log(maps[0][0]);
-  const dirX = [0, 0, 1, 1];
-  const dirY = [1, 1, 0, 0];
+  // 4방향 탐색
+  const dirX = [0, 0, 1, -1];
+  const dirY = [1, -1, 0, 0];
+
+  const DFS = (i, j) => {
+    let queue = [];
+    let day = 0;
+
+    queue.push([i, j]);
+
+    while (queue.length > 0) {
+      const [curi, curj] = queue.shift();
+      day += Number(maps[curi][curj]);
+      visited[curi][curj] = true;
+
+      for (let i = 0; i < 4; i++) {
+        const ni = curi + dirX[i];
+        const nj = curj + dirY[i];
+        if (
+          ni >= 0 &&
+          ni < row &&
+          nj >= 0 &&
+          nj < col &&
+          maps[ni][nj] !== "X" &&
+          !visited[ni][nj]
+        ) {
+          visited[ni][nj] = true;
+          queue.push([ni, nj]);
+        }
+      }
+    }
+    answer.push(day);
+  };
 
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
-      if (maps[i][j] !== "X") {
-        let day = 0;
-        let queue = [];
-        queue.push([i, j]);
-
-        while (queue.length > 0) {
-          const [curI, curJ] = queue.shift();
-          if (maps[curI][curJ] === "X") break;
-
-          day += Number(maps[curI][curJ]);
-          maps[curI][curJ] = "X";
-
-          for (let k = 0; k < 4; k++) {
-            const nextI = curI + dirX[k];
-            const nextJ = curJ + dirY[k];
-            if (nextI < row && nextJ < col && maps[nextI][nextJ])
-              queue.push([nextI, nextJ]);
-          }
-        }
+      if (maps[i][j] !== "X" && !visited[i][j]) {
+        DFS(i, j);
       }
     }
   }
 
-  return answer;
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
 
 console.log(solution(["X591X", "X1X5X", "X231X", "1XXX1"]));
