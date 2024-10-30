007042340458ec6a5fa79ca8bbbbef48282c341c
yuna872 <yunae426@gmail.com>
2024-10-30 14:01:01 +0900

    [Softeer] 지도 자동 구축 - JavaScript

---
+++ Softeer/Lv2/지도자동구축.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = +input.shift();
+
+console.log((2 ** N + 1) ** 2);
