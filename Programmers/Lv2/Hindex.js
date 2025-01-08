2552ae6f241643a9899c5858f692e41be8f050bd
yuna872 <yunae426@gmail.com>
2025-01-08 12:43:49 +0900

    [Programmers] 알고리즘 고득점 kit - 정렬

--- Programmers/Lv2/Hindex.js
+++ Programmers/Lv2/Hindex.js
@@ -1,21 +1,40 @@
-function solution(citations) {
-    var answer = 0;
-    const n = citations.length
-    
-    citations.sort((a, b) => b - a)
-    
-    const maxV = citations[0]
+// function solution(citations) {
+//     var answer = 0;
+//     const n = citations.length
 
-    for(let i = maxV; i >= 0; i--) {
-        // i번 이상 인용된 논문의 수
-        let cnt = citations.filter((h) => h >= i).length
-        if(i <= cnt && n-cnt <= i) {
-            answer = i
-            break
-        }
+//     citations.sort((a, b) => b - a)
+
+//     const maxV = citations[0]
+
+//     for(let i = maxV; i >= 0; i--) {
+//         // i번 이상 인용된 논문의 수
+//         let cnt = citations.filter((h) => h >= i).length
+//         if(i <= cnt && n-cnt <= i) {
+//             answer = i
+//             break
+//         }
+//     }
+
+//     return answer;
+// }
+
+function solution(citations) {
+  let answer = 0;
+  let n = citations.length;
+  citations.sort((a, b) => b - a);
+
+  let maxV = citations[0];
+  for (let h = maxV; h >= 0; h--) {
+    // h번 이상 인용된 논문의 수
+    let cnt = citations.filter((v) => v >= h).length;
+    // h번 이상 인용된 논문의 수가 h번 이상이고 나머지 논문의 수가 h이하
+    if (cnt >= h && n - cnt <= h) {
+      answer = h;
+      break;
     }
-    
-    return answer;
+  }
+
+  return answer;
 }
 
-console.log(solution([3,5,1,0,6]))
+console.log(solution([3, 5, 1, 0, 6]));
--- Programmers/Lv2/가장큰수.js
+++ Programmers/Lv2/가장큰수.js
@@ -1,7 +1,15 @@
+// function solution(numbers) {
+//   var answer = numbers.map((num) => num.toString()).sort((a, b) => (b+a) - (a+b)).join('')
+//   return answer[0] === '0' ? '0' : answer
+// }
+
 function solution(numbers) {
-  var answer = numbers.map((num) => num.toString()).sort((a, b) => (b+a) - (a+b)).join('')
-  return answer[0] === '0' ? '0' : answer
+  let answer = numbers
+    .map(String)
+    .sort((a, b) => b + a - (a + b))
+    .join("");
+  return answer[0] === "0" ? "0" : answer;
 }
 
-console.log(solution([6,10,2]))
-console.log(solution([3,30,34,5,9]))
+console.log(solution([6, 10, 2]));
+console.log(solution([3, 30, 34, 5, 9]));
