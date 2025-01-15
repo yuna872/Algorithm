79662ab107d4f6292c91d625182d035a4f988138
yuna872 <yunae426@gmail.com>
2025-01-15 15:54:49 +0900

    코테 준비 algorithm prac

--- Programmers/Lv2/카펫.js
+++ Programmers/Lv2/카펫.js
@@ -1,12 +1,28 @@
+// function solution(brown, yellow) {
+//   var answer = [];
+
+//   for (let h = 1; h < brown + yellow; h++) {
+//     if ((brown + yellow) % h) continue;
+//     let w = (brown + yellow) / h;
+//     if (w * 2 + h * 2 - 4 === brown) return [w, h];
+//   }
+//   return [0, 0];
+// }
+
 function solution(brown, yellow) {
   var answer = [];
 
   for (let h = 1; h < brown + yellow; h++) {
-    if ((brown + yellow) % h) continue;
-    let w = (brown + yellow) / h;
-    if (w * 2 + h * 2 - 4 === brown) return [w, h];
+    if ((brown + yellow) % h > 0) continue;
+
+    let w = Math.floor((brown + yellow) / h);
+
+    if (2 * w + 2 * h - 4 === brown) {
+      answer = [w, h];
+      break;
+    }
   }
-  return [0, 0];
+  return answer;
 }
 
 console.log(solution(10, 2));
--- Programmers/Lv2/피로도.js
+++ Programmers/Lv2/피로도.js
@@ -1,33 +1,60 @@
+// function solution(k, dungeons) {
+//   var max = 0;
+//   const len = dungeons.length;
+//   let visited = new Array(len).fill(false);
+
+//   const DFS = (cur, power, depth) => {
+//     // 방문 처리
+//     visited[cur] = true;
+//     // 피로도 감소
+//     power -= dungeons[cur][1];
+
+//     for (let i = 0; i < len; i++) {
+//       // 방문한 적이 없고, 최소 피로도 조건을 만족하는 경우에 방문해주기
+//       if (!visited[i] && dungeons[i][0] <= power) {
+//         DFS(i, power, depth + 1);
+//       }
+//     }
+//     // depth의 최대 값을 저장
+//     max = Math.max(max, depth);
+//     // 다음 방문을 위한 방문 기록 복원
+//     visited[cur] = false;
+//   };
+
+//   // 모든 시작점을 고려해야 하기 때문에 순회하며 탐색
+//   for (let i = 0; i < len; i++) {
+//     // 방문할 수 있는 던전인지 확인해주기
+//     if (dungeons[i][0] <= k) DFS(i, k, 1);
+//   }
+
+//   return max;
+// }
+
 function solution(k, dungeons) {
-  var max = 0;
-  const len = dungeons.length;
-  let visited = new Array(len).fill(false);
+  var answer = -1;
+  let N = dungeons.length;
 
-  const DFS = (cur, power, depth) => {
-    // 방문 처리
-    visited[cur] = true;
-    // 피로도 감소
-    power -= dungeons[cur][1];
+  let visited = new Array(N).fill(false);
+  const dfs = (i, curPower, depth) => {
+    visited[i] = true;
+    curPower -= dungeons[i][1];
 
-    for (let i = 0; i < len; i++) {
-      // 방문한 적이 없고, 최소 피로도 조건을 만족하는 경우에 방문해주기
-      if (!visited[i] && dungeons[i][0] <= power) {
-        DFS(i, power, depth + 1);
+    for (let ni = 0; ni < N; ni++) {
+      if (!visited[ni] && dungeons[ni][0] <= curPower) {
+        dfs(ni, curPower, depth + 1);
       }
     }
-    // depth의 최대 값을 저장
-    max = Math.max(max, depth);
-    // 다음 방문을 위한 방문 기록 복원
-    visited[cur] = false;
+
+    answer = Math.max(answer, depth);
+    visited[i] = false;
   };
 
-  // 모든 시작점을 고려해야 하기 때문에 순회하며 탐색
-  for (let i = 0; i < len; i++) {
-    // 방문할 수 있는 던전인지 확인해주기
-    if (dungeons[i][0] <= k) DFS(i, k, 1);
+  // 모든 시작점 고려
+  for (let i = 0; i < N; i++) {
+    if (dungeons[i][0] <= k) dfs(i, k, 1);
   }
 
-  return max;
+  return answer;
 }
 
 console.log(
