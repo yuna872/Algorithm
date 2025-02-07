b1faa80ecdaecae54d3ff64515b8df97831e9f60
yuna872 <yunae426@gmail.com>
2025-02-07 12:09:14 +0900

    [softeer] 징검다리 - JavaScript

--- Softeer/Lv3/징검다리.js
+++ Softeer/Lv3/징검다리.js
@@ -1,27 +1,20 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-const readline = require('readline')
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
-})
+const N = +input.shift();
+let A = input.shift().split(" ").map(Number);
 
-let N = 0
-let rocks = []
-rl.on('line', (line) => {
-    if(!N) N = parseInt(line)
-    else rocks = line.split(' ').map(Number)
-}).on('close', () => {
-    let res = new Array(N).fill(1)
+let dp = new Array(N).fill(1);
 
-    rocks.forEach((rock, now) => {
-        let maxV = 0
-        let i = now - 1
-        while(i >= 0) {
-            if(rock > rocks[i]) maxV = Math.max(maxV, res[i])
-            i--
-        }
-        res[now] = maxV + 1
-    })
+for (let i = 1; i < N; i++) {
+  for (let j = 0; j < i; j++) {
+    if (A[i] > A[j]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
 
-    console.log(Math.max(...res))
-})
+console.log(Math.max(...dp));
